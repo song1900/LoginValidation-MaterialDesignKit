4e3e36fa7fe487e97ef49048c3e71412384a3149
song1900 <woojin1900@gmail.com>
2021-02-04 23:19:20 +0900

    Initial Commit

---
+++ LoginValidation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginValidation
+//
+//  Created by 송우진 on 2021/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
