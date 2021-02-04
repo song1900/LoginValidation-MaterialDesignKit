e23ef2ba9b40082917e59b9fde801e362823e80f
song1900 <woojin1900@gmail.com>
2021-02-04 23:58:05 +0900

    초기 설정 - storyboard 삭제, RootVC 설정

--- LoginValidation/Controllers/RootViewController.swift
+++ LoginValidation/Controllers/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  LoginValidation
 //
 //  Created by 송우진 on 2021/02/04.
@@ -7,13 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        configure()
+    }
+    
+
+    // MARK: - Configure
+    func configure() {
+        view.backgroundColor = .systemBackground
     }
 
-
 }
-
---
+++ LoginValidation/Coordinator.swift
@@ -0,0 +1,21 @@
+//
+//  Coordinator.swift
+//  LoginValidation
+//
+//  Created by 송우진 on 2021/02/04.
+//
+
+import UIKit
+
+class Coordinator {
+    let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        window.rootViewController = RootViewController()
+        window.makeKeyAndVisible()
+    }
+}
