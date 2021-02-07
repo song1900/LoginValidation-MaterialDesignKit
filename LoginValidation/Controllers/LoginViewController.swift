e73bf5c1549e365ade10d415f83d9b52d10eeee3
song1900 <woojin1900@gmail.com>
2021-02-08 00:22:10 +0900

    UIViewController Extensions 추가 ( keyboard )

--- LoginValidation/Controllers/LoginViewController.swift
+++ LoginValidation/Controllers/LoginViewController.swift
@@ -7,14 +7,60 @@
 
 import UIKit
 import MaterialComponents.MaterialTextControls_OutlinedTextFields
+import MaterialComponents.MaterialButtons
 
 class LoginViewController: UIViewController {
-
+    // MARK: - Properties
+    private lazy var idTextField: MDCOutlinedTextField = {
+        let tf = MDCOutlinedTextField()
+        tf.label.text = "id"
+        tf.placeholder = "Enter your id"
+        tf.leadingAssistiveLabel.text = "This is where enter your id"
+        tf.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        tf.sizeToFit()
+        return tf
+    }()
+    
+    private lazy var pwTextField: MDCOutlinedTextField = {
+        let tf = MDCOutlinedTextField()
+        tf.label.text = "password"
+        tf.placeholder = "Enter your password"
+        tf.leadingAssistiveLabel.text = "This is where enter your password"
+        tf.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        tf.sizeToFit()
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    private lazy var loginButton: MDCButton = {
+        let bt = MDCButton()
+        bt.setTitle("login", for: .normal)
+        bt.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        bt.setBackgroundColor(.purple)
+        bt.isUserInteractionEnabled = false
+        bt.alpha = 0.3
+        return bt
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [idTextField, pwTextField, loginButton])
+        sv.axis = .vertical
+        sv.spacing = 50
+        return sv
+    }()
+    
+    // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         configure()
+        
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
     }
     
 
---
+++ LoginValidation/Extensions/UIViewController-extensions.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewController-extensions.swift
+//  LoginValidation
+//
+//  Created by 송우진 on 2021/02/08.
+//
+
+import UIKit
+
+extension UIViewController {
+    func dismissKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func movewViewWhenKeyboardAppearedOrDisappeared() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    
+    @objc private func keyboardWillShow(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        self.view.frame.origin.y = 0 - keyboardSize.height / 2
+    }
+    
+    @objc private func keyboardWillHide(notification: NSNotification) {
+        self.view.frame.origin.y = 0
+    }
+    
+}
